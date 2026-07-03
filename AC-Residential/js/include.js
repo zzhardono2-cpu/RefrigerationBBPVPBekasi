5040e2e3ec8cdc2e2b984ccdfb89b74d8481fd71
zzhardono2-cpu <zzhardono2@gmail.com>
2026-07-03 12:16:32 +0700

    Update include.js

--- AC-Residential/js/include.js
+++ AC-Residential/js/include.js
@@ -1,1 +1,16 @@
-// Load Header, Footer, Sidebar
+// Memuat komponen HTML
+
+async function loadComponent(id, file) {
+    const element = document.getElementById(id);
+
+    if (!element) return;
+
+    const response = await fetch(file);
+    const html = await response.text();
+
+    element.innerHTML = html;
+}
+
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
+loadComponent("sidebar", "components/sidebar.html");
